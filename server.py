b1ad1b242f50648e065056dd1c37ed5a5eb2884c
girikuncoro <girikuncoro@gmail.com>
2015-11-12 02:56:10 -0500

    Added basic handler, including parser and command handlers

--- server.py
+++ server.py
@@ -11,33 +11,16 @@ from threading import Thread
 host = "127.0.0.1"
 port = 8765
 
+
 # error messages to be sent in certain conditions
-# ERROR = {
-#     "syntax": {
-#         "code": "501", "msg": "{} Syntax: proper syntax"},
-#     "order": {
-#         "code": "503", "msg": "{} Error: need {} command"},
-#     "unrecognized": {
-#         "code": "500", "msg": "{} Error: command not recognized"},
-#     "duplicate": {
-#         "code": "503", "msg": "{} Error: duplicate HELO"},
-#     "nested": {
-#         "code": "503", "msg": "{} Error: nested MAIL command"},
-#     "sender": {
-#         "code": "555", "msg": "{} <bad email>: Sender address rejected"},
-#     "recipient": {
-#         "code": "555", "msg": "{} <bad email>: Recipient address invalid"},
-#     "timeout": {
-#         "code": "421", "msg": "{} 4.4.2 gk256 Error: timeout exceeded"}
-# }
 ERROR = {
     "unrecognized": "500 Error: command not recognized",
     "syntax_helo": "501 Syntax: HELO yourhostname",
     "syntax_from": "501 Syntax: MAIL FROM: you@domain.com",
     "syntax_to": "501 Syntax: RCPT TO: sendto@domain.com",
     "order": "503 Error: need {} command",
-    "duplicate": "503 Error: duplicate HELO",
-    "nested": "503 Error: nested MAIL command",
+    "duplicate_helo": "503 Error: duplicate HELO",
+    "nested_mail": "503 Error: nested MAIL command",
     "sender": "555 {}: Sender address rejected",
     "recipient": "555 {}: Recipient address invalid",
     "timeout": "421 4.4.2 gk256 Error: timeout exceeded"
@@ -53,6 +36,8 @@ OK = {
     "FIN": "250 OK: Delivered {} messages"
 }
 
+FINISH = "FIN"
+
 # handle a single client request
 class ConnectionHandler:
     def __init__(self, socket):
@@ -61,30 +46,157 @@ class ConnectionHandler:
         self.socket = socket
         self.s_pointer = 0  # state pointer
         self.states = ["INIT", "HELO", "MAIL", "RCPT", "DATA", "FIN"]
+
+        self.client_name = ""
         self.from_mail = ""
         self.to_mails = []
 
         self.count_msg = 0
+        self.text_body = ""
 
+    # main handler
     def handle(self):
+        self.send_ok("INIT")
+        # print "Connect to {}".format(self.address)
+        while self.states[self.s_pointer] != FINISH:
+            parse_buffer(self.socket.recv(500))
         self.socket.close()
+
+    def parse_buffer(self, messages):
+        args = messages.split("\r\n")
+        self.head_msg = args[0]
+        self.tail_msg = args[1:-1]
+
+        self.parse(head_msg)
+
+        # ****TODO*****
+        # self.parse_msg something
+
+        for m in self.tail_msg:
+            self.parse(m)
+
+    def parse(self, msg):
+        cmd = msg[:4].upper()
+        valid_cmd = False
+
+        if self.curr_state("INIT") and cmd == "HELO":
+            self.handle_helo(msg)
+            valid_cmd = True
+
+        if self.curr_state("HELO") and cmd == "MAIL":
+            self.handle_mail(msg)
+            valid_cmd = True
+
+        if self.curr_state("MAIL") and cmd == "RCPT":
+            self.handle_rcpt(msg)
+            valid_cmd = True
+
+        if self.curr_state("RCPT") and cmd == "RCPT":
+            self.handle_rcpt(msg)
+            valid_cmd = True
+
+        if self.curr_state("RCPT") and cmd == "DATA":
+            self.handle_data(msg)
+            valid_cmd = True
+
+        if self.curr_state("DATA") and msg != ".":
+            self.text_body += "{}\n".format(msg)
+            valid_cmd = True
+
+        if self.curr_state("DATA") and msg == ".":
+            self.send_ok("FIN")
+            #*** TODO****
+            # do backup here
+            self.next_mail()
+            valid_cmd = True
+
+        if not valid_cmd:
+            if self.curr_state("HELO") and cmd == "HELO":
+                self.send_error(ERROR["duplicate_helo"])
+            elif self.curr_state("MAIL") and cmd == "MAIL":
+                self.send_error(ERROR["nested_mail"])
+            elif cmd in self.states:
+                self.send_error(ERROR["order"])
+            else:
+                self.send_error(ERROR["unrecognized"])
+
+    # handle HELO command
+    def handle_helo(self, msg):
+        args = msg.strip().split(" ")
+        if len(args) > 2:
+            self.send_error(ERROR["syntax_helo"])
+        else:
+            self.client_name = args[1]
+            self.send_ok("HELO")
+
+    # handle MAIL FROM command
+    def handle_mail(self, msg):
+        args = msg.strip().split(" ")
+        valid_syntax = False
+
+        if len(args) > 2 and args[0] == "MAIL" and args[1] == "FROM:":
+            valid_syntax = True
+
+        if valid_syntax and valid_mail(args[2]):
+            self.from_mail = args[2]
+            self.send_ok("MAIL")
+
+        if valid_syntax and not valid_mail(args[2]):
+            self.from_mail = "".join(args[2:])
+            self.send_error(ERROR["sender"])
+
+        if not valid_syntax:
+            self.send_error(ERROR["syntax_from"])
+
+    # handle RCPT TO command
+    def handle_rcpt(self, msg):
+        args = msg.strip().split(" ")
+        valid_syntax = False
+
+        if len(args) > 2 and args[0] == "RCPT" and args[1] == "TO:":
+            valid_syntax = True
+
+        if valid_syntax and valid_mail(args[2]):
+            self.to_mails.append(args[2])
+            sel.send_ok("RCPT")
+
+        if valid_syntax and not valid_mail(args[2]):
+            self.to_mails.append("".join(args[2:]))
+            self.send_error(ERROR["recipient"])
+
+        if not valid_syntax:
+            self.send_error(ERROR["syntax_to"])
+
+    # ****** RECHECK,, TODO*****
+    # handle DATA command
+    def handle_data(self, msg):
+        # data is processed inside parse method, only send error/ok here
+        if len(msg) != 4:
+            self.send_error()
+        else:
+            self.send_ok("DATA")
+
+    def curr_state(self, state):
+        if self.states[self.s_pointer] == state:
+            return True
+        return False
+
+    def next_mail(self):
+        self.s_pointer = self.states.index("HELO")
+        self.text_body = ""
+        self.from_mail = ""
+        self.to_mails = []
+
+    def format_text(self):
         pass
 
-    def parse_msg(self):
-        pass
-
-    def parse_bulkmsg(self):
-        pass
-
-    def send_ok(self, curr_state):
-        FINISH = "FIN"
-
-        if curr_state == FINISH:
+    def send_ok(self, curr):
+        if curr == FINISH:
             self.count_msg += 1
 
-        if curr_state in self.states[:-1]:
-            self.s_pointer = self.states.index(curr_state)
-            msg = OK[curr_state]
+        if curr in self.states[:-1]:
+            self.s_pointer = self.states.index(curr)
+            msg = OK[curr]
         else:
             self.s_pointer = len(self.states)-1
             msg = "State not recognized"
@@ -100,39 +212,20 @@ class ConnectionHandler:
 
         if etype not in REQUIRE_ARGS:
             msg = ERROR[etype]
-        elif etype == REQUIRE_ARGS[0]:
+
+        if etype == REQUIRE_ARGS[0]:
             msg = ERROR[etype].format(self.states[self.curr_state+1])
-        elif etype == REQUIRE_ARGS[1]:
+        if etype == REQUIRE_ARGS[1]:
             msg = ERROR[etype].format(self.from_mail)
-        elif etype == REQUIRE_ARGS[2]:
+        if etype == REQUIRE_ARGS[2]:
             msg = ERROR[etype].format(self.to_mails.pop())
 
         # error command will set the new timeout
         self.socket.settimeout(self.socket.gettimeout()/2)
         self.socket.send(msg)
 
-    def format_txt(self):
+    def valid_mail(self):
         pass
-
-    def is_validmail(self):
-        pass
-
-    # handle HELO command
-    def parse_helo(self):
-        pass
-
-    # handle MAIL FROM command
-    def parse_mail_from(self):
-        pass
-
-    # handle RCPT TO command
-    def parse_rcpt_to(self):
-        pass
-
-    # handle DATA command
-    def parse_data(self):
-        pass
-
 
 # thread pool
 class ThreadPool:
